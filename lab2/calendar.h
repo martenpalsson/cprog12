8a779e8db41fb4aa896b81cc195f165e5dd207dc
Simon Österman <simon.oesterman@gmail.com>
2012-11-20 15:05:05 +0100

    haehja

--- lab2/calendar.h
+++ lab2/calendar.h
@@ -82,8 +82,9 @@ struct Calendar{
 				if(it->first != d){
 					break;
 				}
-				if(it->second == event)
+				if(it->second == event){
 					return false;
+				}
 			}
 			events.insert({d, event});
 		} catch(out_of_range){
@@ -92,11 +93,19 @@ struct Calendar{
 		return true;
 	}
 
+	bool remove_event(string event, int day){
+		return remove_event(event, day, today.month(), today.year());
+	}
+
+	bool remove_event(string event, int day, int month){
+		return remove_event(event,day,month,today.year());
+	}
+
 	bool remove_event(string event, int day, int month, int year){
 		try{
 			D d(year,month,day);
 
-			for(auto it = events.find(d); it != events.end(); it++){
+			for(auto it = events.find(d); it != events.end(); ++it){
 				if(it->first != d){
 					break;
 				}
@@ -111,17 +120,12 @@ struct Calendar{
 		return false;
 	}
 	
-	bool remove_event(string event, int day, int month){
-		return remove_event(event,day,month,today.year());
-	}
 	
-	bool remove_event(string event, int day){
-		return remove_event(event, day, today.month(), today.year());
-	}
 	bool remove_event(string event){
-		for(auto it = events.find(today); it != events.end(); it++){
-			if(it->first != today)
+		for(auto it = events.find(today); it != events.end(); ++it){
+			if(it->first != today){
 				break;
+			}
 			if(it->second == event){
 				events.erase(it);
 				return true;
@@ -133,13 +137,10 @@ struct Calendar{
 
 template <typename D>
 ostream & operator<<(ostream & os, const Calendar<D> & cal){
-	D d = cal.today;
-	auto it = cal.events.find(d);
-	++it;
-	for(; it != cal.events.end(); ++it){
-		os << it->first << " : " << it->second << endl;
+	for(auto it = cal.events.begin(); it != cal.events.end(); ++it){
+			if(it->first > cal.today)
+				os << it->first << " : " << it->second << endl;
 	}
-
 	return os;
 }
 }
